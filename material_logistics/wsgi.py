8853798e0061061c03fc7522a7a5ab1a6157c4b2
DamaMichaelYohanna <get2dama11@gmail.com>
2026-06-10 13:01:35 +0100

    added vercel file and requirements

--- material_logistics/wsgi.py
+++ material_logistics/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'material_logistics.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
